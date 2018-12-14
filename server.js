1237ac94695899d6eb6a15a84bba676733db24a4
LiamCoyle <zynk123@hotmail.com>
2018-12-14 12:26:31 +0100

    login

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 let express = require('express');
 
 let bodyParser = require('body-parser');
@@ -15,8 +15,10 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://admin:admin123@ds119988.mlab.com:19988/ripoff');
+mongoose.connect('mongodb://admin:root123@ds249503.mlab.com:49503/integration_front_back');
 var db = mongoose.connection;
 // Setup server port
 var port = process.env.PORT || 8080;
@@ -28,5 +30,32 @@ app.use('/api', require('./routes/api.route'));
 // Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running RestHub on port " + port);
+});*/
+
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 81 : 8080;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
 
---
+++ users/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    mail: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    password: { type: String, required: true },
+    societe: { type: String, required: false },
+    siret: { type: String, required: false },
+    tel: { type: String, required: false },
+    username: { type: String, required: false },
+    adresse: { type: String, required: false },
+    cp: { type: String, required: false },
+    ville: { type: String, required: false },
+    site: { type: String, required: false },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
